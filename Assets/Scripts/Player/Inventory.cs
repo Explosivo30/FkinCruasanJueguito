274f61db05f088504283f5a3540b7b8198316f82
Explosivo30 <edgarhipernovas@gmail.com>
2023-09-20 23:40:35 +0200

    Game Manager Days

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -6,47 +6,5 @@ using UnityEngine;
 public class Inventory
 {
     [SerializeField] int dinero;
-    [SerializeField] List<Objeto> objetos;
-
-    [SerializeField] List<Requerimiento> habilidadesActuales;
-
-    public void AddItemsToBag(Objeto obj)
-    {
-        objetos.Add(obj);
-        if (obj.capacidades) 
-        { 
-            foreach(Requerimiento req in obj.capacidades.Value)
-            {
-                habilidadesActuales.Add(req);
-            }
-        }
-    }
-
-    public void SellItemFromBag(Objeto obj)
-    {
-        
-        if (obj.capacidades)
-        {
-            foreach (Requerimiento req in obj.capacidades.Value)
-            {
-                habilidadesActuales.Add(req);
-            }
-        }
-        objetos.Remove(obj);
-    }
-
-    public int SellAllItems()
-    {
-        int total = 0;
-        for (int i = objetos.Count; i >= 0; i--)
-        {
-            if (!objetos[i].capacidades)
-            {
-                total += objetos[i].sellPrice;
-                objetos.RemoveAt(i);
-            }
-        }
-        dinero += total;
-        return total;
-    }
+   
 }
