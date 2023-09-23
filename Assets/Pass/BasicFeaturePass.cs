ce48cc2dcb556fb4cbb36a888ca3c467f3c4f21a
Hedenrag <otto_may_b-ts3dv1mb2122@cev.com>
2023-09-23 05:38:38 +0200

    Problemas de profundidad arreglados

--- Assets/Pass/BasicFeaturePass.cs
+++ Assets/Pass/BasicFeaturePass.cs
@@ -1,7 +1,4 @@
 using System.Collections.Generic;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Rendering;
-using UnityEngine.Scripting.APIUpdating;
 
 namespace UnityEngine.Rendering.Universal
 {
@@ -39,42 +36,47 @@ namespace UnityEngine.Rendering.Universal
         {
             SortingCriteria sortingCriteria = SortingCriteria.CommonTransparent;
 
+            //Get the camera info of the default rendering pass
             ScriptableRenderer renderer = renderingData.cameraData.renderer;
             RenderTargetIdentifier colorHandle = renderer.cameraColorTargetHandle;
+            RenderTargetIdentifier depthHandle = renderer.cameraDepthTargetHandle;
 
+            //Generate necessary data for the pixel renderer
             DrawingSettings drawingSettings = CreateDrawingSettings(m_ShaderTagIdList, ref renderingData, sortingCriteria);
             ref CameraData cameraData = ref renderingData.cameraData;
             Camera camera = cameraData.camera;
-            Rect pixelRect = camera.pixelRect;
             int pixelWidth = (int)(camera.pixelWidth / pixelDensity);
             int pixelHeight = (int)(camera.pixelHeight / pixelDensity);
             CommandBuffer cmd = CommandBufferPool.Get("BasicFeature");
+
             using (new ProfilingScope(cmd, m_ProfilingSampler))
             {
+                //set the render target and buffers for the pixel renderer
                 cmd.GetTemporaryRT(pixelTexID, pixelWidth, pixelHeight, 0, FilterMode.Point);
                 cmd.GetTemporaryRT(pixelDepthID, pixelWidth, pixelHeight, 24, FilterMode.Point, RenderTextureFormat.Depth);
                 cmd.SetRenderTarget(pixelTexID, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store,
                                     pixelDepthID, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store);
-
+                //clear screen
                 cmd.ClearRenderTarget(true, true, Color.clear);
-
+                //do all of the above and start fresh
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
-
+                //draw the pixel pixel renderer
                 context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref m_FilteringSettings, ref m_RenderStateBlock);
-                cmd.SetRenderTarget(colorHandle, RenderBufferLoadAction.Load, RenderBufferStoreAction.Store);
-
+                //set the render texture back to the default to blend correctly
+                cmd.SetRenderTarget(colorHandle, RenderBufferLoadAction.Load, RenderBufferStoreAction.Store,
+                                    depthHandle, RenderBufferLoadAction.Load, RenderBufferStoreAction.Store);
+                //render everything with the blit shader
                 cmd.Blit(new RenderTargetIdentifier(pixelTexID), BuiltinRenderTextureType.CurrentActive, blitMat);
-
+                //remove buffers to not have memoryLeak
                 cmd.ReleaseTemporaryRT(pixelTexID);
                 cmd.ReleaseTemporaryRT(pixelDepthID);
-
+                //do all of the above and start fresh
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
             }
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
-
     }
 }
