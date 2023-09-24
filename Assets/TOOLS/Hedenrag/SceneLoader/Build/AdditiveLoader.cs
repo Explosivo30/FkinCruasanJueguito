9ed587a79237a5ad200490da09bb06cbd4b4610d
Hedenrag <114000430+Hedenrag@users.noreply.github.com>
2023-09-25 00:20:47 +0200

    Arreglar errores

--- Assets/TOOLS/Hedenrag/SceneLoader/Build/AdditiveLoader.cs
+++ Assets/TOOLS/Hedenrag/SceneLoader/Build/AdditiveLoader.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.TerrainTools;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
